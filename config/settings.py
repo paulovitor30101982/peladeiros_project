eba47556eef9bdeded3d0068a457467fa76d5b25
bobmatheus <bob.rtr@hotmail.com>
2025-08-06 19:52:53 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
